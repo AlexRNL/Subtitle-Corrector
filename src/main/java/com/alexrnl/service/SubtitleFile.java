46b1223cf8093d2aa21ab07480ad1cd9729c3991
Alex Barféty <alex.barfety@free.fr>
2013-01-16 23:35:53 +0100

    Start completing the subtitle file class.
    May need refactoring, is it logical make a class for a TreeSet, and no
    many other properties?

--- src/main/java/com/alexrnl/service/SubtitleFile.java
+++ src/main/java/com/alexrnl/service/SubtitleFile.java
@@ -2,22 +2,39 @@ package com.alexrnl.service;
 
 import java.nio.file.Path;
 import java.util.NavigableSet;
+import java.util.TreeSet;
 import java.util.logging.Logger;
 
 /**
  * TODO
  * @author Alex
  */
-public class SubtitleFile<T extends Subtitle> {
+public class SubtitleFile {
 	/** Logger */
-	private static Logger	lg	= Logger.getLogger(SubtitleFile.class.getName());
+	private static Logger					lg	= Logger.getLogger(SubtitleFile.class.getName());
 	
-	/** The name of the file is represent */
-	private String			fileName;
 	/** The actual file which is represented */
-	private Path			subtitle;
+	private final Path						file;
 	/** The list of subtitles of a file */
-	private NavigableSet<T>	subtitles;
-	/** If <code>true</code>, */
-	private boolean			sync;
+	private final NavigableSet<Subtitle>	subtitles;
+	
+	public SubtitleFile (final Path file) {
+		super();
+		this.file = file;
+		this.subtitles = new TreeSet<>();
+	}
+	
+	/**
+	 * Add a subtitle to the file.
+	 * @param subtitle
+	 *        the subtitle to add.
+	 */
+	public void addSubtitle (final Subtitle subtitle) {
+		if (subtitle == null) {
+			lg.warning("Cannot add null subtitle");
+			return;
+		}
+		subtitles.add(subtitle);
+	}
+	
 }
