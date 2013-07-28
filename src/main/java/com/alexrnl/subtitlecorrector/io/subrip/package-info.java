1e76b27b592e89a5974bc773b045ba1327b63361
Alex Barféty <alexbarfety@free.fr>
2013-07-28 19:54:48 +0200

    Add package for SubRip format

---
+++ src/main/java/com/alexrnl/subtitlecorrector/io/subrip/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package with the classes for reading and writing
+ * <a href="https://en.wikipedia.org/wiki/SubRip#SubRip_text_file_format">SubRip</a> file format.
+ * @author Alex
+ */
+package com.alexrnl.subtitlecorrector.io.subrip;
