d9b239d28af735c8e23f5e0fe78ba8db765a7831
Alex Barféty <alexbarfety@free.fr>
2013-07-29 23:02:26 +0200

    Add basic test for reading a subtitle file and a sample file

--- src/test/java/com/alexrnl/subtitlecorrector/io/subrip/SubRipReaderTest.java
+++ src/test/java/com/alexrnl/subtitlecorrector/io/subrip/SubRipReaderTest.java
@@ -2,20 +2,29 @@ package com.alexrnl.subtitlecorrector.io.subrip;
 
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import com.alexrnl.subtitlecorrector.common.SubtitleFile;
 
 /**
  * Test suite for the {@link SubRipReader} class.
  * @author Alex
  */
 public class SubRipReaderTest {
+	/** The reader to use for the tests */
+	private SubRipReader reader;
 	
 	/**
 	 * Set up test attributes.
 	 */
 	@Before
 	public void setUp () {
+		reader = new SubRipReader();
 	}
 	
 	/**
@@ -27,10 +36,17 @@ public class SubRipReaderTest {
 	}
 	
 	/**
-	 * Test method for {@link com.alexrnl.subtitlecorrector.io.SubtitleReader#readFile(java.nio.file.Path)}.
+	 * Test method for
+	 * {@link com.alexrnl.subtitlecorrector.io.SubtitleReader#readFile(java.nio.file.Path)}.
+	 * @throws URISyntaxException
+	 *         if the syntax of the files to test is not valid.
+	 * @throws IOException
+	 *         if the reading failed.
 	 */
 	@Test
-	public void testReadFile () {
-		fail("Not yet implemented"); // TODO
+	public void testReadFile () throws IOException, URISyntaxException {
+		final SubtitleFile subtitles = reader.readFile(Paths.get(getClass().getResource("/Suits - 03x01 - The Arrangement.EVOLVE.English.C.updated.Addic7ed.com.srt").toURI()));
+		
+		System.out.println(subtitles);
 	}
 }
