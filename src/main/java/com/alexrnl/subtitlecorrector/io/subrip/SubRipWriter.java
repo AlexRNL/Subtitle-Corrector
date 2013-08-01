51551a1d27f7fc699982a6356b19223159da6214
Alex Barféty <alexbarfety@free.fr>
2013-08-01 20:30:33 +0200

    Partial implementation of SubRipWriter

---
+++ src/main/java/com/alexrnl/subtitlecorrector/io/subrip/SubRipWriter.java
@@ -0,0 +1,63 @@
+package com.alexrnl.subtitlecorrector.io.subrip;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.alexrnl.subtitlecorrector.common.Subtitle;
+import com.alexrnl.subtitlecorrector.common.SubtitleFile;
+import com.alexrnl.subtitlecorrector.io.SubtitleWriter;
+
+/**
+ * Writer for the SubRip format.<br />
+ * @author Alex
+ */
+public class SubRipWriter extends SubtitleWriter {
+	/** Logger */
+	private static Logger			lg	= Logger.getLogger(SubRipWriter.class.getName());
+	
+	/** The date formatter */
+	private final SimpleDateFormat	dateFormat;
+	/** The subtitle counter (required by SubRip format) */
+	private Integer					subtitleCounter;
+	
+	/**
+	 * Constructor #1.<br />
+	 */
+	public SubRipWriter () {
+		super();
+		dateFormat = new SimpleDateFormat(SubRip.SUBRIP_DATE_FORMAT);
+		subtitleCounter = null;
+	}
+	
+	@Override
+	protected void writeHeader (final SubtitleFile file, final BufferedWriter writer) throws IOException {
+		if (subtitleCounter != null) {
+			throw new IllegalStateException("It seems that the previous file was not finished fully writen");
+		}
+		subtitleCounter = 0;
+	}
+
+	@Override
+	protected void writeFooter (final SubtitleFile file, final BufferedWriter writer) throws IOException {
+		if (lg.isLoggable(Level.INFO)) {
+			lg.info("Successfully writen " + subtitleCounter + " subtitles");
+		}
+		subtitleCounter = null;
+	}
+
+	@Override
+	protected void writeSubtitle (final Subtitle subtitle, final BufferedWriter writer) throws IOException {
+		// TODO write byte order mark for UTF-8
+		writer.write(++subtitleCounter);
+		// TODO newline
+		writer.write(dateFormat.format(subtitle.getBegin()));
+		writer.write(SubRip.SUBRIP_DATE_SEPARATOR);
+		writer.write(dateFormat.format(subtitle.getEnd()));
+		// TODO newline
+		writer.write(subtitle.getContent());
+		// TODO newline
+	}
+}
