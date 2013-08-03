57d5dd4d0a10969385af8fad55bae1d0e3a8c7cb
Alex Barféty <alexbarfety@free.fr>
2013-08-03 20:32:34 +0200

    Add methods to Strategy interface

--- src/main/java/com/alexrnl/subtitlecorrector/correctionstrategy/Strategy.java
+++ src/main/java/com/alexrnl/subtitlecorrector/correctionstrategy/Strategy.java
@@ -1,9 +1,17 @@
 package com.alexrnl.subtitlecorrector.correctionstrategy;
 
+import java.util.List;
+
+import com.alexrnl.subtitlecorrector.common.Subtitle;
+
 /**
- * TODO
+ * Interface for a given strategy to correct the subtitles.
  * @author Alex
  */
 public interface Strategy {
+	
+	List<Parameter> getParameters ();
+	
+	void correct (Subtitle subtitle);
 	
 }
