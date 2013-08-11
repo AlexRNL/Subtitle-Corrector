268f9b6fb48ff123dbf81ca36be7f1e76a2fef64
Alex Barféty <alexbarfety@free.fr>
2013-08-11 23:37:23 +0200

    Add different kind of dictionaries in the DictionaryManager

--- src/main/java/com/alexrnl/subtitlecorrector/service/DictionaryManager.java
+++ src/main/java/com/alexrnl/subtitlecorrector/service/DictionaryManager.java
@@ -1,6 +1,10 @@
 package com.alexrnl.subtitlecorrector.service;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -14,14 +18,23 @@ public class DictionaryManager {
 	/** Logger */
 	private static Logger	lg	= Logger.getLogger(DictionaryManager.class.getName());
 	
-	/** Map with the dictionaries */
-	private final Map<String, Dictionary> dictionaries;
+	/** Map with the dictionary for each locale */
+	private final Map<Locale, Dictionary>	localeDictionaries;
+	/** Map with the custom dictionaries of the user */
+	private final Map<String, Dictionary>	customDictionaries;
+	/** The dictionary of the current session */
+	private final Dictionary				sessionDictionary;
 	
 	/**
 	 * Constructor #1.<br />
+	 * @throws IOException
+	 *         if a dictionary could not be created.
 	 */
-	public DictionaryManager () {
+	public DictionaryManager () throws IOException {
 		super();
-		this.dictionaries = new HashMap<>();
+		localeDictionaries = new HashMap<>();
+		customDictionaries = new HashMap<>();
+		sessionDictionary = new Dictionary(Files.createTempFile("sessionDictionary", ".txt"),
+				Charset.defaultCharset(), true);
 	}
 }
