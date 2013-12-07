7fef9f9d25ea73292022e13196907ef6ba0653af
Alex Barféty <alexbarfety@free.fr>
2013-12-07 01:28:09 +0100

    Update ConsoleUSerPrompt to allow custom input/output
    so it can be tested with unit tests.

--- src/main/java/com/alexrnl/subtitlecorrector/gui/view/ConsoleUserPrompt.java
+++ src/main/java/com/alexrnl/subtitlecorrector/gui/view/ConsoleUserPrompt.java
@@ -1,5 +1,6 @@
 package com.alexrnl.subtitlecorrector.gui.view;
 
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.logging.Logger;
@@ -17,42 +18,56 @@ public class ConsoleUserPrompt implements UserPrompt {
 	/** Logger */
 	private static Logger		lg	= Logger.getLogger(ConsoleUserPrompt.class.getName());
 	
-	/** The scanner plugged on the console input */
-	private Scanner				input;
+	/** The console input stream */
+	private final InputStream	input;
+	/** The scanner plugged on the console inputScanner */
+	private Scanner				inputScanner;
 	/** The console output */
 	private final PrintStream	output;
 	
 	/**
 	 * Constructor #1.<br />
+	 * @param input
+	 *        the input stream to use for reading the user's answers.
+	 * @param output
+	 *        the output to use for displaying information to the user.
+	 */
+	public ConsoleUserPrompt (final InputStream input, final PrintStream output) {
+		super();
+		this.input = input;
+		this.output = output;
+	}
+	
+	/**
+	 * Constructor #2.<br />
+	 * Build a {@link ConsoleUserPrompt} with the {@link System#in} and {@link System#out}.
 	 */
 	public ConsoleUserPrompt () {
-		super();
-		input = null;
-		output = System.out;
+		this(System.in, System.out);
 	}
 	
 	@Override
 	public void startSession (final SessionParameters parameters) {
-		if (input != null) {
-			throw new IllegalStateException("Session was not properly stop, input was not null");
+		if (inputScanner != null) {
+			throw new IllegalStateException("Session was not properly stop, inputScanner was not null");
 		}
-		input = new Scanner(System.in);
+		inputScanner = new Scanner(input);
 	}
 
 	@Override
 	public void stopSession () {
-		if (input == null) {
-			throw new IllegalStateException("Session was not properly started, input was null");
+		if (inputScanner == null) {
+			throw new IllegalStateException("Session was not properly started, inputScanner was null");
 		}
-		input.close();
-		input = null;
+		inputScanner.close();
+		inputScanner = null;
 		
 	}
 
 	@Override
 	public UserPromptAnswer confirm (final String context, final Word original, final String replacement) {
-		if (input == null) {
-			throw new IllegalStateException("Session was not properly started, input is null, " +
+		if (inputScanner == null) {
+			throw new IllegalStateException("Session was not properly started, inputScanner is null, " +
 					"cannot confirm replacement");
 		}
 		String answer;
@@ -66,19 +81,19 @@ public class ConsoleUserPrompt implements UserPrompt {
 			output.println(context);
 		}
 		output.println("y/n >");
-		final boolean keep = 'y' == input.nextLine().charAt(0);
+		final boolean keep = 'y' == inputScanner.nextLine().charAt(0);
 		if (keep) {
 			answer = replacement;
 		} else {
 			output.println("Type the replacement:");
-			answer = input.nextLine();
+			answer = inputScanner.nextLine();
 			if (answer.isEmpty()) {
 				cancelled = true;
 			}
 		}
 		output.println("Remember choice for the rest of the session?");
 		output.println("y/n >");
-		rememberChoice = 'y' == input.nextLine().charAt(0);
+		rememberChoice = 'y' == inputScanner.nextLine().charAt(0);
 		
 		return new UserPromptAnswer(answer, cancelled, rememberChoice);
 	}
