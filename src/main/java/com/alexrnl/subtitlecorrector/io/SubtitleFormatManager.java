d4d6a9cc422adcd81d17724d8415fc60fb2e0356
Alex Barféty <alexbarfety@free.fr>
2014-02-09 21:24:53 +0100

    Start implementation of class SubtitleFormatManager

--- src/main/java/com/alexrnl/subtitlecorrector/io/SubtitleFormatManager.java
+++ src/main/java/com/alexrnl/subtitlecorrector/io/SubtitleFormatManager.java
@@ -1,21 +1,100 @@
 package com.alexrnl.subtitlecorrector.io;
 
-import java.util.Map;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.alexrnl.commons.io.IOUtils;
+import com.alexrnl.commons.utils.StringUtils;
 import com.alexrnl.subtitlecorrector.common.SubtitleFile;
 
 /**
  * Manager to ease the use of the {@link SubtitleFile} reading / writing.<br />
- * TODO
  * @author Alex
  */
 public class SubtitleFormatManager {
 	/** Logger */
-	private static Logger	lg	= Logger.getLogger(SubtitleFormatManager.class.getName());
+	private static Logger				lg	= Logger.getLogger(SubtitleFormatManager.class.getName());
 	
-	/** The subtitle readers */
-	private Map<String, SubtitleReader> readers;
-	/** The subtitle writers */
-	private Map<String, SubtitleWriter> writers;
+	/** The registered format of subtitles */
+	private final Set<SubtitleFormat>	formats;
+	
+	/**
+	 * Constructor #1.<br />
+	 * Default constructor.
+	 */
+	public SubtitleFormatManager () {
+		super();
+		formats = new HashSet<>();
+	}
+	
+	/**
+	 * Register a new {@link SubtitleFormat}.
+	 * @param format
+	 *        the format to add.
+	 * @return <code>true</code> if a previous same format existed (and is thus override).
+	 */
+	public boolean registerFormat (final SubtitleFormat format) {
+		return formats.add(format);
+	}
+	
+	/**
+	 * Find a subtitle format by its name.
+	 * @param name
+	 *        the name of the format to find.
+	 * @return the matching format or <code>null</code> if no matching format were found.
+	 */
+	public SubtitleFormat getFormatByName (final String name) {
+		if (StringUtils.nullOrEmpty(name)) {
+			throw new IllegalArgumentException("Cannot search for null or empty name");
+		}
+		
+		for (final SubtitleFormat format : formats) {
+			if (format.getName().equals(name)) {
+				return format;
+			}
+		}
+		
+		if (lg.isLoggable(Level.INFO)) {
+			lg.info("No matching format found for name " + name);
+		}
+		return null;
+	}
+	
+	/**
+	 * Find the {@link SubtitleFormat} which support the specified extension.
+	 * @param extension
+	 *        the extension of the file.
+	 * @return the format which support the following extension.
+	 */
+	public Set<SubtitleFormat> getFormatByExtension (final String extension) {
+		if (StringUtils.nullOrEmpty(extension)) {
+			throw new IllegalArgumentException("Cannot search for null or empty extension");
+		}
+		
+		final Set<SubtitleFormat> candidates = new HashSet<>();
+		for (final SubtitleFormat format : formats) {
+			if (format.getExtensions().contains(extension)) {
+				candidates.add(format);
+			}
+		}
+		
+		if (lg.isLoggable(Level.INFO)) {
+			lg.info("Subtitle format matching extension " + extension + " are: " + candidates);
+		}
+		return candidates;
+	}
+	
+	/**
+	 * Find the {@link SubtitleFormat} which support the following {@link Path} according to its
+	 * extension.
+	 * @param path
+	 *        the path of the file.
+	 * @return the format(s) which support the following path.
+	 */
+	public Set<SubtitleFormat> getFormatByPath (final Path path) {
+		return getFormatByExtension(IOUtils.getFileExtension(path));
+	}
 }
