92bec33c1558896be4d6210034403acd293bef0d
Alex Barféty <alexbarfety@free.fr>
2015-07-21 22:08:37 +0200

    Use SimpleDialog class from commons library

--- src/main/java/com/alexrnl/subtitlecorrector/gui/view/GraphicUserPrompt.java
+++ src/main/java/com/alexrnl/subtitlecorrector/gui/view/GraphicUserPrompt.java
@@ -3,15 +3,14 @@ package com.alexrnl.subtitlecorrector.gui.view;
 
 import static com.alexrnl.subtitlecorrector.common.TranslationKeys.KEYS;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
 
 import com.alexrnl.commons.gui.swing.SwingUtils;
 import com.alexrnl.commons.translation.Dialog;
+import com.alexrnl.commons.translation.SimpleDialog;
 import com.alexrnl.commons.translation.Translatable;
 import com.alexrnl.commons.translation.Translator;
 import com.alexrnl.commons.utils.StringUtils;
@@ -42,76 +41,6 @@ public class GraphicUserPrompt extends SessionStateAdapter implements UserPrompt
 		super();
 	}
 	
-	private final class UserPromptDialog implements Dialog {
-		/** The title of the prompt */
-		private final String				title;
-		/** The message of the prompt */
-		private final String				message;
-		/** The parameters of the prompt */
-		private final Collection<Object>	parameters;
-
-		/**
-		 * Constructor #1.<br />
-		 * @param title
-		 *        the title of the prompt.
-		 * @param message
-		 *        the message of the prompt.
-		 * @param parameters
-		 *        the parameters of the prompt.
-		 */
-		public UserPromptDialog (final String title, final String message, final Collection<Object> parameters) {
-			super();
-			this.title = title;
-			this.message = message;
-			this.parameters = parameters;
-		}
-		
-		/**
-		 * Constructor #2.<br />
-		 * @param title
-		 *        the title of the prompt.
-		 * @param message
-		 *        the message of the prompt.
-		 * @param parameters
-		 *        the parameters of the prompt.
-		 */
-		public UserPromptDialog (final String title, final String message, final Object... parameters) {
-			this(title, message, Arrays.asList(parameters));
-		}
-		
-		/**
-		 * Constructor #3.<br />
-		 * @param title
-		 *        the title of the prompt.
-		 * @param message
-		 *        the message of the prompt.
-		 */
-		public UserPromptDialog (final String title, final String message) {
-			this(title, message, Collections.emptyList());
-		}
-
-		@Override
-		public String title () {
-			return title;
-		}
-		
-		@Override
-		public String message () {
-			return message;
-		}
-		
-		@Override
-		public Collection<Object> getParameters () {
-			return parameters;
-		}
-		
-		@Override
-		public String getTranslationKey () {
-			return message();
-		}
-		
-	}
-	
 	@Override
 	public void setTranslator (final Translator translator) {
 		this.translator = translator;
@@ -137,36 +66,36 @@ public class GraphicUserPrompt extends SessionStateAdapter implements UserPrompt
 	
 	@Override
 	public void information (final String translationKey, final Object... parameters) {
-		showDialog(new UserPromptDialog("title", translationKey, parameters), JOptionPane.INFORMATION_MESSAGE);
+		showDialog(new SimpleDialog("title", translationKey, parameters), JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	@Override
 	public void warning (final String translationKey, final Object... parameters) {
-		showDialog(new UserPromptDialog("title", translationKey, parameters), JOptionPane.WARNING_MESSAGE);
+		showDialog(new SimpleDialog("title", translationKey, parameters), JOptionPane.WARNING_MESSAGE);
 		
 	}
 	
 	@Override
 	public void error (final String translationKey, final Object... parameters) {
-		showDialog(new UserPromptDialog("title", translationKey, parameters), JOptionPane.ERROR_MESSAGE);
+		showDialog(new SimpleDialog("title", translationKey, parameters), JOptionPane.ERROR_MESSAGE);
 	}
 	
 	@Override
 	public <T extends Translatable> T askChoice (final Collection<T> choices, final String translationKey,
 			final Object... parameters) {
-		return SwingUtils.askChoice(null, translator, new UserPromptDialog("title", translationKey, parameters), choices, MAXIMUM_LINE_LENGTH);
+		return SwingUtils.askChoice(null, translator, new SimpleDialog("title", translationKey, parameters), choices, MAXIMUM_LINE_LENGTH);
 	}
 	
 	@Override
 	public UserPromptAnswer confirm (final String context, final Word original, final String replacement) {
 		final com.alexrnl.subtitlecorrector.common.TranslationKeys.Gui.UserPrompt userPromptKey = KEYS.gui().userPrompt();
 		final Dialog dialog = StringUtils.neitherNullNorEmpty(context) ?
-				new UserPromptDialog("title", userPromptKey.replaceWithContext(), original, replacement, context) :
-				new UserPromptDialog("title", userPromptKey.replace(), original, replacement);
+				new SimpleDialog("title", userPromptKey.replaceWithContext(), original, replacement, context) :
+				new SimpleDialog("title", userPromptKey.replace(), original, replacement);
 		final String answer = SwingUtils.askFreeInput(null, translator, dialog, replacement, MAXIMUM_LINE_LENGTH);
 		
 		return new UserPromptAnswer(answer, answer == null,
-				SwingUtils.askConfirmation(null, translator, new UserPromptDialog("title", userPromptKey.rememberChoice()), MAXIMUM_LINE_LENGTH));
+				SwingUtils.askConfirmation(null, translator, new SimpleDialog("title", userPromptKey.rememberChoice()), MAXIMUM_LINE_LENGTH));
 	}
 	
 	@Override
